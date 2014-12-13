d71b43fe4f51c8ae585852e3ac3bcf11a3ad7188
pamtrak06 <pamtrak06@gmail.com>
2014-12-13 18:02:37 +0100

    correction of unicode in mapacache.py

--- mapcache.py
+++ mapcache.py
@@ -13,19 +13,19 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"w:p:",["wms=","prj="])
     except getopt.GetoptError:
-        print 'mapcache.py -wms <wms url> -prj <project name>'
+        print "mapcache.py -wms <wms url> -prj <project name>";
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-h":
-            print 'mapcache.py -wms <wms url> -prj <project name>';
+            print "mapcache.py -wms <wms url> -prj <project name>";
             sys.exit();
         elif opt in ("-w", "-wms", "--wms"):
             wmsUrl = arg;
         elif opt in ("-p", "-prj", "--prj"):
             project = arg;
 
-    logging.info('WMS url is "', wmsUrl);
-    logging.info('Project name is "', project);
+    logging.info(u"WMS url is ", wmsUrl);
+    logging.info(u"Project name is ", project);
 
     wms = WebMapService(wmsUrl, version=u"1.1.1")
     #wms.identification.type
@@ -36,38 +36,38 @@ def main(argv):
     source = u"";
     concat = u"&amp;";
     urlRequestCapa = wmsUrl;
-    idx = urlRequestCapa.index('?');
+    idx = urlRequestCapa.index(u"?");
     if idx == -1:
         concat = u"?";
 
     mapcache = u"&lt;?xml version=\"1.0\" encoding=\"UTF-8\"?&gt;\n";
     mapcache += u"&lt;mapcache&gt;\n";
     mapcache += u"  &lt;metadata&gt;\n";
-    mapcache += u"    &lt;title&gt;Mapcache service for url: ' + urlRequestCapa + '&lt;/title&gt;\n";
-    mapcache += u"    &lt;abstract&gt;Render map services for url: ' + urlRequestCapa + '&lt;/abstract&gt;\n";
+    mapcache += u"    &lt;title&gt;Mapcache service for url: " + urlRequestCapa + u"&lt;/title&gt;\n";
+    mapcache += u"    &lt;abstract&gt;Render map services for url: " + urlRequestCapa + u"&lt;/abstract&gt;\n";
     mapcache += u"  &lt;/metadata&gt;\n\n";
 
     # Cache: Disk
     mapcache += u"  &lt;cache name=\"disk\" type=\"disk\"&gt;\n";
-    mapcache += u"    &lt;base&gt;/tmp/' + project + '&lt;/base&gt;\n";
+    mapcache += u"    &lt;base&gt;/tmp/" + project + u"&lt;/base&gt;\n";
     mapcache += u"    &lt;symlink_blank/&gt;\n";
     mapcache += u"  &lt;/cache&gt;\n\n";
 
     # Cache: template
     mapcache += u"  &lt;cache name=\"tmpl\" type=\"disk\"&gt;\n";
-    mapcache += u"    &lt;base&gt;/tmp' + project + '&lt;/base&gt;\n";
-    mapcache += u"    &lt;template&gt;/tmp/' + project + '/template/{tileset}#{grid}#{dim}/{z}/{x}/{y}.{ext}&lt;/template&gt;\n";
+    mapcache += u"    &lt;base&gt;/tmp" + project + u"&lt;/base&gt;\n";
+    mapcache += u"    &lt;template&gt;/tmp/" + project + u"/template/{tileset}#{grid}#{dim}/{z}/{x}/{y}.{ext}&lt;/template&gt;\n";
     mapcache += u"  &lt;/cache&gt;\n\n";
 
     # Cache: sqlite
     mapcache += u"  &lt;cache name=\"sqlite\" type=\"sqlite3\"&gt;\n";
-    mapcache += u"    &lt;dbfile&gt;/tmp/' + project + '/sqlitetiles.db&lt;/dbfile&gt;\n";
+    mapcache += u"    &lt;dbfile&gt;/tmp/" + project + u"/sqlitetiles.db&lt;/dbfile&gt;\n";
     mapcache += u"    &lt;pragma name=\"key\"&gt;value&lt;/pragma&gt;\n";
     mapcache += u"  &lt;/cache&gt;\n\n";
 
     # Cache: MbTiles
     mapcache += u"  &lt;cache name=\"mbtiles\" type=\"mbtiles\"&gt;\n";
-    mapcache += u"    &lt;dbfile&gt;/tmp/' + project + '/' + project + '.mbtiles&lt;/dbfile&gt;\n";
+    mapcache += u"    &lt;dbfile&gt;/tmp/" + project + u"/" + project + u".mbtiles&lt;/dbfile&gt;\n";
     mapcache += u"  &lt;/cache&gt;\n\n";
 
     # Format: png
@@ -96,34 +96,34 @@ def main(argv):
     for layername in wms.contents:
         layer = wms.contents[layername];
 
-        logging.info('Layer:' + layer.name);
-        logging.info('\tAvailable elevations: '.join(layer.elevations));
-        logging.info('\tAvailable times: '.join(layer.timepositions));
+        logging.info("Layer:" + layer.name);
+        logging.info("\tAvailable elevations: ".join(layer.elevations));
+        logging.info("\tAvailable times: ".join(layer.timepositions));
 
         arrayTab = layer.styles;
         for sname in arrayTab:
             style = arrayTab[sname];
 
-            tileset = layer.name + "_" + sname;
-            source = u"SOURCE_" + layer.name + "_" + sname;
+            tileset = layer.name + u"_" + sname;
+            source = u"SOURCE_" + layer.name + u"_" + sname;
 
             mapcache += u"  &lt;!-- Source for layer: " + layer.name + u", style: " + sname + u" --&gt;\n";
             mapcache += u"  &lt;source name=\"" + source + u"\" type=\"wms\"&gt;\n";
             mapcache += u"    &lt;http&gt;\n";
-            mapcache += u"      &lt;url&gt;' + urlRequestCapa + '&lt;/url&gt;\n";
+            mapcache += u"      &lt;url&gt;" + urlRequestCapa + u"&lt;/url&gt;\n";
             mapcache += u"    &lt;/http&gt;\n";
             mapcache += u"    &lt;getmap&gt;\n";
             mapcache += u"      &lt;params&gt;\n";
             mapcache += u"        &lt;FORMAT&gt;image/png&lt;/FORMAT&gt;\n";
-            mapcache += u"        &lt;LAYERS&gt;' + layer.name + '&lt;/LAYERS&gt;\n";
-            mapcache += u"        &lt;STYLES&gt;' + sname + '&lt;/STYLES&gt;\n";
+            mapcache += u"        &lt;LAYERS&gt;" + layer.name + u"&lt;/LAYERS&gt;\n";
+            mapcache += u"        &lt;STYLES&gt;" + sname + u"&lt;/STYLES&gt;\n";
             mapcache += u"      &lt;/params&gt;\n";
             mapcache += u"    &lt;/getmap&gt;\n";
             mapcache += u"  &lt;/source&gt;\n\n";
 
-            mapcache += u"  &lt;!-- Tileset for layer: ' + layer.name + ', style: ' + sname + ' --&gt;\n";
+            mapcache += u"  &lt;!-- Tileset for layer: " + layer.name + u", style: " + sname + u" --&gt;\n";
             mapcache += u"  &lt;tileset name=\"" + tileset + u"\"&gt;\n";
-            mapcache += u"    &lt;source&gt;' + source + '&lt;/source&gt;\n";
+            mapcache += u"    &lt;source&gt;" + source + u"&lt;/source&gt;\n";
             mapcache += u"    &lt;dimensions&gt;\n";
             mapcache += u"      &lt;dimension type=\"regex\" name=\"elevation\" default=\"-1\"&gt;.*&lt;/dimension&gt;\n";
             mapcache += u"      &lt;dimension type=\"regex\" name=\"time\" default=\"" + layer.defaulttimeposition + u"\"&gt;.*&lt;/dimension&gt;\n";
@@ -135,7 +135,7 @@ def main(argv):
             # projs = layer.srs;
             #for p in xrange(0, projs.length):
             #    proj = projs[p];
-            #    mapcache += u"    &lt;grid&gt;' + proj + '&lt;/grid&gt;\n";
+            #    mapcache += u"    &lt;grid&gt;" + proj + u"&lt;/grid&gt;\n";
 
             mapcache += u"    &lt;grid&gt;WGS84&lt;/grid&gt;\n";
             mapcache += u"    &lt;grid&gt;GoogleMapsCompatible&lt;/grid&gt;\n";
@@ -151,14 +151,14 @@ def main(argv):
         arrayTab = layer.styles;
         for sname in arrayTab:
             style = arrayTab[sname];
-            tileset = layer.name + '_' + sname;
-            source = u"SOURCE_" + layer.name + "_" + sname;
+            tileset = layer.name + u"_" + sname;
+            source = u"SOURCE_" + layer.name + u"_" + sname;
             mapcache += u"    &lt;forwarding_rule name=\"RULE" + tileset + u"&gt;\n";
             mapcache += u"      &lt;param name=\"SERVICE\" type=\"values\"&gt;WMS&lt;/param&gt;\n";
-            mapcache += u"      &lt;param name=\"LAYERS\" type=\"values\"&gt;' + layer.name + '&lt;/param&gt;\n";
-            mapcache += u"      &lt;param name=\"STYLES\" type=\"values\"&gt;' + sname + '&lt;/param&gt;\n";
+            mapcache += u"      &lt;param name=\"LAYERS\" type=\"values\"&gt;" + layer.name + u"&lt;/param&gt;\n";
+            mapcache += u"      &lt;param name=\"STYLES\" type=\"values\"&gt;" + sname + u"&lt;/param&gt;\n";
             mapcache += u"      &lt;http&gt;\n";
-            mapcache += u"        &lt;url&gt;' + urlRequestCapa + concat + 'LAYERS=' + tileset + '&lt;/url&gt;\n";
+            mapcache += u"        &lt;url&gt;" + urlRequestCapa + concat + u"LAYERS=" + tileset + u"&lt;/url&gt;\n";
             mapcache += u"      &lt;/http&gt;\n";
             mapcache += u"    &lt;/forwarding_rule&gt;\n";
 
@@ -178,7 +178,7 @@ def main(argv):
 
     # General parameters
     mapcache += u"  &lt;errors&gt;report&lt;/errors&gt;\n";
-    mapcache += u"  &lt;lock_dir&gt;/tmp/' + project + '&lt;/lock_dir&gt;\n";
+    mapcache += u"  &lt;lock_dir&gt;/tmp/" + project + u"&lt;/lock_dir&gt;\n";
     mapcache += u"  &lt;threaded_fetching&gt;true&lt;/threaded_fetching&gt;\n";
     mapcache += u"  &lt;log_level&gt;debug&lt;/log_level&gt;\n";
     mapcache += u"  &lt;auto_reload&gt;true&lt;/auto_reload&gt; \n";
@@ -187,9 +187,9 @@ def main(argv):
     mapcache += u"&lt;/mapcache&gt;\n";
 
     escmapcache = unescape(mapcache);
-    #print escmapcache;
-    #fmapcache = ET.fromstring(ET.tostring(escmapcache.encode('utf8'), method='xml'));
-    #tree = ET.XML(escmapcache.encode('utf8'));
+    print escmapcache;
+    #fmapcache = ET.fromstring(ET.tostring(escmapcache.encode("utf8"), method="xml"));
+    #tree = ET.XML(escmapcache.encode("utf8"));
     with open("mapcache.xml", "w") as f:
         #f.write(ET.tostring(tree))
         f.write(escmapcache)
