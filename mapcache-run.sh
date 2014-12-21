b1241961378b2b15fb9dc36fb41ef16d39a4ac8f
pamtrak <pamtrak06@gmail.com>
2014-12-22 00:02:04 +0100

    add script to build mapcache.xml and restart apache

---
+++ mapcache-run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+url=$1
+cache=$2
+
+if [ $# -eq 0 ]
+  then
+
+    echo 'ERROR : no arguments supplied'
+    echo 'mapcache-run --wms \"wms url\" --prj \"folder name for cache\"'
+
+  else
+    if [ -z "$1" ] || [ -z "$2" ]
+      then
+
+        echo 'ERROR : argument(s) could not be empty'
+        echo 'mapcache-run --wms \"wms url\" --prj \"folder name for cache\"'
+
+      else
+
+        cd /etc/apache/conf-available
+
+        # generate mapcache.xml from capabilities
+        python mapcache.py --wms $url --prj $cache
+
+        # restart apache and mapcache module
+        apachectl restart
+
+    fi
+fi
