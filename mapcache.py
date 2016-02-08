d1d10c151af84fe4067963d3de5777e95a88ea7e
pamtrak06 <pamtrak06@gmail.com>
2016-02-09 00:37:40 +0100

    add WARRANTY

--- mapcache.py
+++ mapcache.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+# ---------------------------------------------------------------------------
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# ---------------------------------------------------------------------------
+
 from __future__ import unicode_literals
 import sys, getopt, logging
 from owslib.wms import WebMapService
